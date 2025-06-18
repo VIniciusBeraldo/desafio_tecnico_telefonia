8b207f5072757c927a91b9bf9a0cde490161e5b5
Vinicius Beraldo <viniciusberaldo96@gmail.com>
2025-06-18 11:31:57 -0300

    Atualizando projeto

--- app/app.py
+++ app/app.py
@@ -1,82 +1,93 @@
 import os, cv2, threading
 import numpy as np
 from flask import Flask, request, render_template, jsonify
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
-# --- Configurações para Upload ---
-# Define o diretório onde os arquivos serão salvos temporariamente
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'avi', 'mov'} # Adicione os tipos de arquivo que você permite
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mp4'} 
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Cria o diretório de uploads se ele não existir
-# Isso garante que a pasta 'uploads' esteja pronta quando o app iniciar
+# Caso não exista o diretorio para o armazenamento dos arquivos, o mesmo sera criado.
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+# Verificando se os arquivos estão dentro das extensões permtidas.
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Configurando o socekio para buscar as informações enviadas ao redis quando a trehad emite um template_found.
 socketio = SocketIO(app, message_queue='redis://localhost:6379/0', cors_allowed_origins="*")
 
-def detectacao_template(video_path, image_path, sid=None):
+def detectacao_template(video_path, image_path, sid=None, threshold=0.7):
 
-    # Carregar o template
-    template = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    try:
+        # Carregamando a imagem template
+        template = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        video_base = cv2.VideoCapture(video_path)
 
-    # Verificar se o template foi carregado
-    if template is None:
-        print("Verifique se o arquivo existe e o caminho está correto.")
-        if sid:
-            socketio.emit('processing_error', {'message': 'Erro ao carregar template.'}, room=sid)
-        return
+        # Verificar se o vídeo foi aberto corretamente
+        if not video_base.isOpened():
+            if sid:
+                socketio.emit('processing_error', {'message': 'Erro ao abrir vídeo.'}, room=sid)
+            return
 
-    cap = cv2.VideoCapture(video_path)
+        frame_count = 0
 
-    # Verificar se o vídeo foi aberto corretamente
-    if not cap.isOpened():
-        print("Verifique se o arquivo de vídeo existe, o caminho está correto e se ele não está corrompido.")
-        if sid:
-            socketio.emit('processing_error', {'message': 'Erro ao abrir vídeo.'}, room=sid)
-        return
+        while True:
+            
+            ret, frame = video_base.read()
 
-    frame_count = 0
-    while True:
-        
-        ret, frame = cap.read()
+            if not ret: # caso nã encontre nenhum frame, finaliza o processamento.
+                print("Fim do vídeo ou erro ao ler frame.")
+                break
 
-        if not ret:
-            print("Fim do vídeo ou erro ao ler frame.")
-            break
+            frame_count += 1
 
-        frame_count += 1
+            frame_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        frame_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            res = cv2.matchTemplate(frame_cinza, template, cv2.TM_CCOEFF_NORMED)
 
-        res = cv2.matchTemplate(frame_cinza, template, cv2.TM_CCOEFF_NORMED)
+            loc = np.where(res >= threshold)
 
-        threshold = 0.7
+            num_deteccoes_no_frame = 0
 
-        loc = np.where(res >= threshold)
+            for pt in zip(*loc[::-1]):
+                num_deteccoes_no_frame += 1
 
-        num_deteccoes_no_frame = 0
+            if num_deteccoes_no_frame > 0:
+                print(f"Template detectado no Frame {frame_count}!")
+                if sid: 
+                    message_data = {
+                        'message': f"Template detectado no Frame {frame_count}"
+                    }
+                    socketio.emit('template_found', message_data, room=sid) 
 
-        for pt in zip(*loc[::-1]): # Itera sobre todas as detecções encontradas
-            num_deteccoes_no_frame += 1
-
-        if num_deteccoes_no_frame > 0:
-            print(f"Template detectado no Frame {frame_count}!")
-            if sid: # Se tivermos um SID (ID da sessão WebSocket do cliente)
+        if num_deteccoes_no_frame == 0:
+            if sid: 
                 message_data = {
-                    'message': f"Template detectado no Frame {frame_count}!",
+                    'message': f"Template não encontrado."
                 }
-                socketio.emit('template_found', message_data, room=sid) # Envia a mensagem apenas para este cliente
-                print(f"Evento 'template_found' emitido para SID: {sid} no frame {frame_count}")
+                socketio.emit('template_found', message_data, room=sid) 
 
+    except Exception as e:
+        if sid:
+            socketio.emit('processing_error', {'message': f"Erro inesperado: {str(e)}"}, room=sid)
+    finally:
+        if 'cap' in locals() and video_base.isOpened():
+            video_base.release()
+
+        if sid: # Notifica que o processamento terminou
+            socketio.emit('processing_complete', {'message': 'Processamento de vídeo concluído.'}, room=sid)
+        
+        if os.path.exists(video_path):
+            os.remove(video_path)
+        if os.path.exists(image_path):
+            os.remove(image_path)
+            
 # --- Rotas da Aplicação ---
 
 @app.route('/')
@@ -97,6 +108,8 @@ def upload_files():
     image_path = None
 
     client_ws_sid = request.form.get('ws_sid')
+    threshold     = request.form.get('threshold')
+    threshold_value = float(threshold)
 
     if not client_ws_sid:
         print("Aviso: SID do WebSocket não recebido no upload. Mensagens WebSocket não serão enviadas para um cliente específico.")
@@ -116,12 +129,12 @@ def upload_files():
     # Após salvar, você pode usar video_path e image_path para seu processamento com OpenCV
     # Por exemplo:
     if video_path and image_path:
-        thread = threading.Thread(target=detectacao_template, args=(video_path, image_path))
+        thread = threading.Thread(target=detectacao_template, args=(video_path, image_path, client_ws_sid, threshold_value))
         thread.start()
 
-        return jsonify({"message": "Arquivos recebidos e processamento iniciado em background!"}), 200
+        return jsonify({"message": "Processamento iniciado."}), 200
     else:
-        return 'Erro ao salvar um ou ambos os arquivos.', 500
+        return jsonify({'error': 'Erro ao salvar um ou ambos os arquivos.'}), 500 # Retorno JSON
     
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,10 +1,67 @@
 const socket = io('http://localhost:5000');
 
-const messagesList = document.getElementById('messages'); // Onde as mensagens serão exibidas na HTML
-const wsSidInput = document.getElementById('wsSid');     // O campo oculto para guardar o SID
-const uploadForm = document.getElementById('upload_form'); // Seu formulário
+const listaFrames = document.getElementById('frames_encontrados');
+const wsSidInput = document.getElementById('wsSid');     
+const uploadForm = document.getElementById('upload_form'); 
+const paragrafoStatusServidor = document.getElementById('status_servidor'); 
+const paragrafoStatusProcessamento = document.getElementById('status_processamento'); 
+const thresholdInput = document.getElementById('threshold_input')
 
-socket.on('template_found', (data) => {
-    console.log("encontrou")
-    addMessage(msg, 'detection'); 
+socket.on('connect', () => {
+    wsSidInput.value = socket.id; // Guarda o ID da sessão WebSocket no campo oculto.
+    atualizarStatusServidor('Conectado ao servidor!', 'info'); 
+});
+
+function atualizarStatusServidor(msg, type='info') {  // função destinada a aterar o status do servidor.
+    paragrafoStatusServidor.textContent = msg
+}
+
+function atualizarStatusProcessamento(msg, type='info') {  // função destinada a aterar o status do processamento.
+    paragrafoStatusProcessamento.textContent = msg
+}
+
+function adicionarFramesEncontrados(msg, type = 'info') { // função destinada a incrementar a lista dos frames encontrados.
+    const li = document.createElement('li');
+    li.classList.add('frames_encontrados', type);
+    li.textContent = msg;
+    listaFrames.appendChild(li);
+}
+
+socket.on('template_found', (data) => { // acionado o evento de que o template foi encontrado é chamado a função adicionarFramesEncontrados para incrementar o li om a mensagem passada.
+    adicionarFramesEncontrados(data.message, 'detection'); 
+});
+
+socket.on('processing_complete', (data) => { // Quando o processamento do vídeo termina é atualizado o <p> do satus de processamento.
+    atualizarStatusProcessamento(data.message, 'success');
+});
+
+uploadForm.addEventListener('submit', async (event) => { // evento ao clicar em enviar, para realiar o pedido para flask e depois realizar o processamento com opencv que esta em app.py
+    event.preventDefault(); 
+
+    listaFrames.innerHTML = ''
+
+    const formData = new FormData();
+    formData.append('video', document.getElementById('arquivo_video').files[0]);
+    formData.append('image', document.getElementById('arquivo_template').files[0]);
+    formData.append('ws_sid', wsSidInput.value);
+    formData.append('threshold', thresholdInput.value);
+    
+    atualizarStatusProcessamento('Enviando arquivos para processamento...', 'info');
+
+    try {
+        const response = await fetch('/uploads', {
+            method: 'POST',
+            body: formData
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            atualizarStatusProcessamento(result.message, 'info');
+        } else {
+            atualizarStatusProcessamento(`Erro no upload: ${result.error}`, 'error');
+        }
+    } catch (error) {
+        atualizarStatusServidor(`Erro de rede ou servidor: ${error.message}`, 'error'); 
+    }
 });
